92db25fe61cc4025eaccbbfcdad5ee72c4b3120d
unknown <SHERMAN@SHERMAN-LAPPY2.(none)>
2012-02-15 01:58:19 -0800

    Update to Q5
    Comment out

--- multiagent/multiAgents.py
+++ multiagent/multiAgents.py
@@ -268,7 +268,7 @@ def betterEvaluationFunction(currentGameState):
     DESCRIPTION: <write something here so we know what you did>
   """
   "*** YOUR CODE HERE ***"
-  util.raiseNotDefined()
+  #util.raiseNotDefined()
 
 # Abbreviation
 better = betterEvaluationFunction
